872c5179fd421c7333631460da71c8a3bba05381
Arsens <arsens.morins@chi.lv>
2018-11-10 17:05:09 +0200

    First

---
+++ app/src/main/java/com/hfad/bitsandpizzas/Pizza.java
@@ -0,0 +1,28 @@
+package com.hfad.bitsandpizzas;
+
+/**
+ * Created by davidg on 04/05/2017.
+ */
+
+public class Pizza {
+    private String name;
+    private int imageResourceId;
+
+    public static final Pizza[] pizzas = {
+            new Pizza("Diavolo", R.drawable.diavolo),
+            new Pizza("Funghi", R.drawable.funghi),
+            new Pizza("Studentu", R.drawable.studentu)
+    };
+
+    private Pizza(String name, int imageResourceId) {
+        this.name = name;
+        this.imageResourceId = imageResourceId;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+}
